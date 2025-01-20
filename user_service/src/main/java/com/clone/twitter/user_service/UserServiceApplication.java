cdb1897b3ec8cdcad5bd3a28a9bb1ba1092c1e78
Evgenij Pashkeciv <epaskevic4@gmail.com>
2025-01-20 09:42:49 +0300

    initial commit

---
+++ user_service/src/main/java/com/clone/twitter/user_service/UserServiceApplication.java
@@ -0,0 +1,33 @@
+package com.clone.twitter.user_service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
+
+@SpringBootApplication
+@EnableFeignClients("com.clone.twitter.user_service.client")
+@EnableRetry
+@OpenAPIDefinition(
+        info = @Info(
+                title = "User Service",
+                version = "1.0.0")
+)
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
+}
---
+++ user_service/src/main/java/com/clone/twitter/user_service/client/FeignConfig.java
@@ -0,0 +1,15 @@
+package com.clone.twitter.user_service.client;
+
+
+import com.clone.twitter.user_service.config.context.UserContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public FeignUserInterceptor feignUserInterceptor(UserContext userContext) {
+        return new FeignUserInterceptor(userContext);
+    }
+}
